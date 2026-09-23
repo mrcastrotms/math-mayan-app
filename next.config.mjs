5d26783cde75b3bcfb71402132d7c4d827a2a9b5
César Castro <cesar015.2016@gmail.com>
2026-09-23 14:26:49 -0700

    feat(config): inject build timestamp and env variables into next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_VERCEL_ENV: process.env.VERCEL_ENV || "development",
+    NEXT_PUBLIC_BUILD_TIME: new Date().toISOString(),
   },
   /* config options here */
 };
